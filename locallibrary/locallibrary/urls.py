7e6ff07c1727c43f090126e5a23ac72e478d45a1
Hope Tambala <hpbala@umich.edu>
2019-09-16 22:07:04 +0000

    Models tutorial complete

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -20,11 +20,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Use include() to add paths from the catalog application
 from django.urls import include
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
 
+#Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='/catalog/')),
