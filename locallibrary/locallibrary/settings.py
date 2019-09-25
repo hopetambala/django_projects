90946dd757a35ab64605b4dc7e1652f7fd3c07cf
Hope Tambala <hpbala@umich.edu>
2019-09-25 10:58:24 +0000

    fix(): where CSV is load

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    'django_extensions',
+    'unesco.apps.UnescoConfig',
 ]
 
 MIDDLEWARE = [
