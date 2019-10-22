a563351d4eb413d3bd331ee719b7266b3d6db5e5
Hope Tambala <hpbala@umich.edu>
2019-10-22 13:03:12 +0000

    chore: add cats

---
+++ dj4e/cats/views.py
@@ -0,0 +1,97 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+from cats.models import Cat, Breed
+from cats.forms import MakeForm
+
+# Create your views here.
+
+class MainView(LoginRequiredMixin, View) :
+    def get(self, request):
+        mc = Breed.objects.all().count();
+        al = Cat.objects.all();
+
+        ctx = { 'breed_count': mc, 'cat_list': al };
+        return render(request, 'cats/cat_list.html', ctx)
+
+class BreedView(LoginRequiredMixin,View) :
+    def get(self, request):
+        ml = Breed.objects.all();
+        ctx = { 'breed_list': ml };
+        return render(request, 'cats/breed_list.html', ctx)
+
+# We use reverse_lazy() because we are in "constructor code"
+# that is run before urls.py is cmopletely loaded
+class BreedCreate(LoginRequiredMixin, View):
+    template = 'cats/breed_form.html'
+    success_url = reverse_lazy('cats:all')
+    def get(self, request) :
+        form = MakeForm()
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request) :
+        form = MakeForm(request.POST)
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        make = form.save()
+        return redirect(self.success_url)
+
+class BreedUpdate(LoginRequiredMixin, View):
+    model = Breed
+    success_url = reverse_lazy('cats:all')
+    template = 'cats/breed_form.html'
+    def get(self, request, pk) :
+        breed = get_object_or_404(self.model, pk=pk)
+        form = MakeForm(instance=breed)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk) :
+        breed = get_object_or_404(self.model, pk=pk)
+        form = MakeForm(request.POST, instance = breed)
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        form.save()
+        return redirect(self.success_url)
+
+class BreedDelete(LoginRequiredMixin, DeleteView):
+    model = Breed
+    success_url = reverse_lazy('autos:all')
+    template = 'cats/breed_confirm_delete.html'
+
+    def get(self, request, pk) :
+        breed = get_object_or_404(self.model, pk=pk)
+        form = MakeForm(instance=breed)
+        ctx = { 'make': make }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk) :
+        breed = get_object_or_404(self.model, pk=pk)
+        breed.delete()
+        return redirect(self.success_url)
+
+# Take the easy way out on the main table
+class CatCreate(LoginRequiredMixin,CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
+
+class CatDelete(LoginRequiredMixin, DeleteView):
+    model = Cat
+    fields = '__all__'
+    success_url = reverse_lazy('cats:all')
